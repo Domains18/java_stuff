5488c7df9b3840f6f6017ce67811fcbee2d5602e
Domains18 <gibsonsgibson88@gmail.com>
2023-12-11 16:57:16 +0300

    Next14

---
+++ src/app/pages/checkout/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Checkout() {
+    return (
+        <div>
+            <h1>Checkout</h1>
+        </div>
+    )
+}
---
+++ src/app/pages/layout.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Layout({ children }: any) {
+    return (
+        <div>
+            <h1>Layout</h1>
+            {children}
+        </div>
+    )
+}
