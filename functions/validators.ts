08fb799ee989b000e06fbb5656eb8ea051aaf9e5
Domains18 <gibsonsgibson88@gmail.com>
2024-03-03 16:37:04 +0000

    3 Mar 2024, 19:37

--- functions/validators.ts
+++ functions/validators.ts
@@ -8,5 +8,8 @@ export async function validateTransactions(req: Request, res: Response,) {
         return res.sendStatus(400);
     }
     const MerchantRequestID = payload.MerchantRequestID;
-    const transaction = await prismaConstants.transaction.findFirst({})
+    const transaction = await prismaConstants.transaction.findFirst({ MerchantRequestID: MerchantRequestID });
+    if (transaction == null) {
+        return res.status(404).json({ message: "Transaction not found" });
+    }
 }
