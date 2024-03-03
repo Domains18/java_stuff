5a6c95f53527832100486b2d46b87db4576ce10c
Domains18 <gibsonsgibson88@gmail.com>
2024-03-03 16:09:47 +0000

    3 Mar 2024, 19:09

--- functions/validators.ts
+++ functions/validators.ts
@@ -2,4 +2,10 @@ import { Request, Response } from "express";
 import prismaConstants from "../constants/consts";
 
 
-export async function validateTransactions(req:Request, res:Response, ){}
+export async function validateTransactions(req: Request, res: Response,) {
+    const payload = req.body;
+    if (!payload) {
+        return res.sendStatus(400);
+    }
+    const MerchantRequestID = payload.MerchantRequestID;
+}
