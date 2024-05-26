b366c7444442a02be45e42f0d379882ba0fb5861
Domains18 <gibsonsgibson88@gmail.com>
2024-05-26 19:51:24 +0300

    chore: Remove unnecessary files and code

--- constants/consts.ts
+++
@@ -1,6 +0,0 @@
-
-import { PrismaClient } from "@prisma/client/extension";
-
-const prismaConstants = new PrismaClient();
-
-export default prismaConstants;
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Domains18/clientele
+
+go 1.22.2
+
+require github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
